ca7b1480f441611c50de9d9f844176c57269f488
Rômulo Mendes Figueiredo <romulomendesfigueiredo@gmail.com>
2024-11-24 18:13:58 -0300

    código enviado

--- day01/day01.py
+++ day01/day01.py
@@ -1,3 +1,8 @@
-linha1 = input()
-print("Hello, World.")
-print(linha1, end="")
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string, end="")
