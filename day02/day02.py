c507bee71b687564f1f66b2e88a13d4ac3cfd43b
Rômulo Mendes Figueiredo <romulomendesfigueiredo@gmail.com>
2024-11-25 18:29:04 -0300

    dia 02

--- day02/day02.py
+++ day02/day02.py
@@ -17,6 +17,9 @@ import sys
 
 def solve(meal_cost, tip_percent, tax_percent):
     # Write your code here
+    tip = meal_cost * tip_percent / 100
+    tax = meal_cost * tax_percent / 100
+    print(round(meal_cost + tip + tax), end="")
 
 if __name__ == '__main__':
     meal_cost = float(input().strip())
